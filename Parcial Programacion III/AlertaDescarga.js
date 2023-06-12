87786d493cce488aa0de6f0f9540c772caa052fc
maxuis <99999132+maxuis@users.noreply.github.com>
2023-06-12 15:48:25 -0300

    Add files via upload

---
+++ Parcial Programacion III/AlertaDescarga.js
@@ -0,0 +1,5 @@
+function mostrarAlerta() {
+  var fechaDescarga = new Date("2023-06-12");
+  var mensaje = "La descarga estará disponible a partir del " + fechaDescarga.toLocaleDateString();
+  alert(mensaje);
+}                    
---
+++ Parcial Programacion III/Respuestas.js
@@ -0,0 +1,15 @@
+function toggleAnswer(index) {
+    var answers = [
+      "GamesFull es una plataforma de juegos en línea donde los usuarios pueden descargar y jugar diversos títulos.",
+      "Existen varias razones para escoger a GamesFull, como su amplia selección de juegos, facilidad de uso y precios competitivos.",
+      "Sí, GamesFull es una plataforma confiable con años de experiencia en la industria de los videojuegos.",
+      "Sí, en GamesFull existen reglas que deben seguirse para garantizar una experiencia segura y justa para todos los usuarios.",
+      "Los miembros de GamesFull son jugadores de diferentes partes del mundo que disfrutan de los juegos ofrecidos en la plataforma.",
+      "Puedes comunicarte con los administradores de GamesFull a través de su correo electrónico de soporte o mediante el formulario de contacto en su sitio web.",
+      "GamesFull aloja sus servidores en ubicaciones seguras y confiables para garantizar un rendimiento óptimo y una experiencia de juego sin interrupciones.",
+      "Sí, en GamesFull existe la opción de realizar donaciones para apoyar el desarrollo y mantenimiento de la plataforma."
+    ];
+  
+    var answer = answers[index];
+    alert(answer);
+  }
